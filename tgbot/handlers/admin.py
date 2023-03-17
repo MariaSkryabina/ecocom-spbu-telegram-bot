ef9d223d6d04323f5a06ed6c4e1cea629db7381b
Maria Skryabina <skryabina.mary98@gmail.com>
2023-03-17 16:23:49 +0300

    Added handler for user feedback and support reply

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -2,13 +2,36 @@ from aiogram import Dispatcher
 from aiogram.types import Message
 
 
-async def admin_start(message: Message):
+def extract_id(message: Message) -> int:
     """
+    Извлекает ID юзера из хэштега в сообщении
+    :param message: сообщение, из хэштега в котором нужно достать айди пользователя
+    :return: ID пользователя, извлечённый из хэштега в сообщении
+    """
+    # Получение списка сущностей (entities) из текста или подписи к медиафайлу в отвечаемом сообщении
+    entities = message.entities or message.caption_entities
+    # Если всё сделано верно, то последняя (или единственная) сущность должна быть хэштегом...
+    if not entities or entities[-1].type != "hashtag":
+        raise ValueError("Не удалось извлечь ID для ответа!")
 
-    :type message: object
-    """
-    await message.reply("Hello, admin!")
+    # ... более того, хэштег должен иметь вид #id123456, где 123456 — ID получателя
+    hashtag = entities[-1].get_text(message.text or message.caption)
+    if len(hashtag) < 4 or not hashtag[3:].isdigit():  # либо просто #id, либо #idНЕЦИФРЫ
+        raise ValueError("Некорректный ID для ответа!")
+
+    return int(hashtag[3:])
+
+
+async def reply_to_user(message: Message):
+
+    # Вырезаем ID
+    try:
+        user_id = extract_id(message.reply_to_message)
+    except ValueError as ex:
+        return await message.reply(str(ex))
+
+    await message.copy_to(user_id)
 
 
 def register_admin(dp: Dispatcher):
-    dp.register_message_handler(admin_start, commands=["start"], state="*", is_admin=True)
+    dp.register_message_handler(reply_to_user, state='*', is_reply=True, is_support=True)
